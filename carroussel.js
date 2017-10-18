960c61a0b366803ea68f72235809a6c4344fd42c
Benjamin Jaeger <benjaminjaeger31@gmail.com>
2017-10-18 23:16:14 +0200

    Add files via upload

---
+++ carroussel.js
@@ -0,0 +1,53 @@
+$(document).ready(function () {
+    var $carrousel = $('#carrousel'),
+        $img = $('.img-carrousel'),
+        indexImg = $img.length - 1;
+        i = 0;
+        $currentImg = $img.eq(i);
+
+        $img.css('display','none');
+        $currentImg.css('display', 'block');
+
+        //$carrousel.append('<div class="controls"><span class="prev">Précédent</span><span class="next">Suivant</span></div>');
+        $('.next').click(function () {
+            i++;
+            if (i <= indexImg ){
+                $img.css('display', 'none');
+                $currentImg = $img.eq(i);
+                $currentImg.css('display', 'block');
+            }else{
+                i = indexImg;
+            }
+        
+        });
+
+        $('.prev').click(function () {
+            i--;
+            if (i>=0){
+                $img.css('display', 'none');
+                $currentImg = $img.eq(i);
+                $currentImg.css('display', 'block');
+            }else{
+                i = 0;
+            }
+
+        });
+
+        function slideImg(){
+            setTimeout(function () {
+                if (i<indexImg){
+                    i++;
+                }else{
+                    i = 0;
+                }
+
+                $img.css('display', 'none');
+                $currentImg = $img.eq(i);
+                $currentImg.css('display', 'block');
+
+                slideImg();
+
+            }, 3000);
+        }
+slideImg();
+});
